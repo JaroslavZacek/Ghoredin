2ca514635679929e262f5fc01906b79e897b71ac
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-15 17:58:47 +0200

    Update App.jsx
    Upravena logika na App.

--- Ghoredin/client-web/src/App.jsx
+++ Ghoredin/client-web/src/App.jsx
@@ -1,8 +1,29 @@
+import { useAuth } from "./features/auth/AuthContext";
 import AuthPage from "./features/auth/components/AuthPage";
 
 function App() {
+  const { user, loading, logout} = useAuth();
+
+  if (loading) {
+    return (
+      <p style={{ textAlign: "center", marginTop: 48}}>
+        Načítání...
+      </p>
+    );
+  }
+
+  if (!user) {
+    return <AuthPage />
+  }
+
   return (
-    <AuthPage />
+    <div style={{ maxWidth: 600, margin: "40px auto", fontFamily: "var(--font-sans)"}}>
+      <h1>Kronika Ghoredinu</h1>
+
+      <p>Přihlášen jako: {user.userId}</p>
+
+      <button className="auth-button">Odhlásit se</button>
+    </div>
   );
 }
 
