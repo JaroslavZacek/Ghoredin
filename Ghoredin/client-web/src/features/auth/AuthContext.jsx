4d06511506fe2b4bea9105640045a94b6a0f40d4
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-15 17:14:23 +0200

    Create AuthContext.jsx
    Vytvoření AuthContext pro uchování informací o přihlášeném uživately. Také přidán hook useAuth, díky kterému budeš mít přístu kde všemu při zavolání v const

---
+++ Ghoredin/client-web/src/features/auth/AuthContext.jsx
@@ -0,0 +1,54 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+import { login as apiLogin, getMe} from "./api/authApi";
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const me = await getMe();
+
+                setUser(me);
+            }
+            catch {
+                setUser(null);
+            }
+            finally {
+                setLoading(false);
+            }
+        };
+
+        checkAuth();
+    }, []);
+
+    const login = async (email, password) => {
+        await apiLogin(email, password);
+
+        const me = await getMe();
+
+        setUser(me);
+    };
+
+    const logout = async () => {
+        setUser(null);
+    };
+
+    const value = { user, loading, login, logout };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+    const context = useContext(AuthContext);
+
+    if (context === null) {
+        throw new Error("useAuth musí být uvnitř AuthProvider");
+    }
+
+    return context;
+}
