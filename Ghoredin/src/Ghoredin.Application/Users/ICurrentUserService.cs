76cbb9321cbd1b0982bc89004e43e3a03db284db
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-15 12:07:45 +0200

    Create Database
    Vytvoření migrace a databáze

---
+++ Ghoredin/src/Ghoredin.Application/Users/ICurrentUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Users
+{
+    public interface ICurrentUserService
+    {
+        string? UserId { get; }
+
+        bool IsAuthenticated { get; }
+    }
+}
