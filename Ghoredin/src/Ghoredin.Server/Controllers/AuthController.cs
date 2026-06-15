e03f7ce3227287d56b091b30eeb88998b6228fdf
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-15 17:28:20 +0200

    Create AuthController.cs
    Vytvoření AuthController.cs. Přidána metoda Logout pro odhlášení

---
+++ Ghoredin/src/Ghoredin.Server/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Ghoredin.Infrastructure.Identity;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ghoredin.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        /// <summary>
+        /// Odhlásí aktuálně přihlášeného uživatele.
+        /// </summary>
+        /// <returns>Asynchronní úkol vracející HTTP 200 OK při úspěšném odhlášení.</returns>
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok();
+        }
+    }
+}
