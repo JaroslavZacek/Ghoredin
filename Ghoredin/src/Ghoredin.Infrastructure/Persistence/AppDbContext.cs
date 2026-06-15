21fb942c975639e0977ec56bf6475296e0e8d1e9
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-15 03:30:12 +0200

    Create AppDbContext.cs
    Vytvoření Identity navádění

---
+++ Ghoredin/src/Ghoredin.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Ghoredin.Infrastructure.Identity;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Infrastructure.Persistence
+{
+    public class AppDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
