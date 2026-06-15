ab09d75189bad00366df23f51c6d504151d3c4dd
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-15 12:24:13 +0200

    Create CurrentUserService.cs
    Vytvoření CurrentUserService v Ghoredin.Server/Servcices/

---
+++ Ghoredin/src/Ghoredin.Server/Services/CurrentUserService.cs
@@ -0,0 +1,20 @@
+﻿using Ghoredin.Application.Users;
+
+using System.Security.Claims;
+
+namespace Ghoredin.Server.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        public bool IsAuthenticated => _httpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated ?? false;
+    }
+}
