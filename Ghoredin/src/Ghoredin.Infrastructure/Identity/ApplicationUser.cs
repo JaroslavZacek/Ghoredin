37ddfd7848ad8e10d2c22f6f152d4ff0c9cc57ae
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-15 03:28:52 +0200

    Create ApplicationUser.cs
    Vytvoření ApplicationUser pro Identity.

---
+++ Ghoredin/src/Ghoredin.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
