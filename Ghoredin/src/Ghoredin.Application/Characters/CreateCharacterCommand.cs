8a4d49b013f6b3b8736d8c1532dd34e0e34a4fbd
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-17 21:00:59 +0200

    Create CreateCharacterCommand.cs
    Vytvořena record třída CreateCharacterCommand.

---
+++ Ghoredin/src/Ghoredin.Application/Characters/CreateCharacterCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Characters
+{
+    public record CreateCharacterCommand(
+        string Name,
+        string GameSystemId,
+        Dictionary<string, object> SheetData
+
+     );
+}
