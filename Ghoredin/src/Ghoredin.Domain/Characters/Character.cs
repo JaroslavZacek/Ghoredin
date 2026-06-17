9d6ddce6b0868b394985a3d20e5a935dbe39b08d
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-17 17:58:23 +0200

    Create Character.cs
    Vytvořena třída character, která bude mít informace o postavách hráčů. Vlastní vlastnost GameSystem, která určuje do jakého systému postava patří, s tím souvisí i dictionery SheetData, který si uchovává informace jaké další vlastnosti postava bude mít, což určuje právě GameSystem.

---
+++ Ghoredin/src/Ghoredin.Domain/Characters/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Domain.Characters
+{
+    public class Character
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string GameSystem { get; set; } = string.Empty;
+
+        public string OwnerUserId { get; set; } = string.Empty;
+        public Guid? CampaignId { get; set; }
+
+        public string? PortraitUrl { get; set; }
+
+        public Dictionary<string, object> SheetData { get; set; } = new();
+
+    }
+}
