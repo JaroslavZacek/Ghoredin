9f23ad0fec801b323d87563f6aaf285ed97cfac6
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-17 18:07:39 +0200

    Create ICharacterRepository.cs
    Vytvořena třída ICharacterRepository.cs v Application vrstvě.

---
+++ Ghoredin/src/Ghoredin.Application/Characters/ICharacterRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ghoredin.Domain.Characters;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Characters
+{
+    public class ICharacterRepository
+    {
+        Task<Character> GetByIdAsync(Guid id);
+        Task<List<Character>> GetByOwnerAsync(string ownerUserId);
+        Task AddAsync(Character character);
+        Task SaveChangesAsync();
+    }
+}
