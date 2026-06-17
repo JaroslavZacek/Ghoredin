68195abb55bf4c4eff8afaf3d228cd50d9fcb4eb
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-17 21:04:09 +0200

    Create ICharacterService.cs
    Vytvořeno rozhraní ICharacterService

---
+++ Ghoredin/src/Ghoredin.Application/Characters/ICharacterService.cs
@@ -0,0 +1,15 @@
+﻿using Ghoredin.Domain.Characters;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Characters
+{
+    public interface ICharacterService
+    {
+        Task<Character> CreateAsync(CreateCharacterCommand command);
+        Task<List<Character>> GetMyCharacterAsync();
+        Task<Character> GetByIdAsync(Guid id);
+    }
+}
