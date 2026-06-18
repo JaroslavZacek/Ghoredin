76b4231f54ca975f75264e2f961fe2bbd215ca81
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-18 22:26:49 +0200

    Create Campaign.cs
    Vytvořená třída pro uložení dobrodružství.

---
+++ Ghoredin/src/Ghoredin.Domain/Campaigns/Campaign.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Domain.Campaigns
+{
+    public class Campaign
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string GameSystemId { get; set; } = string.Empty;
+
+        public int? MaxPlayers { get; set; }
+
+        public List<CampaignMember> Members { get; set; } = new();
+
+        public Dictionary<string, object> GameSystemSettings { get; set; } = new();
+    }
+}
