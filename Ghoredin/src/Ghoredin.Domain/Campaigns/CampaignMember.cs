f7c858d9f8da1f53559ff71802afce5cf2e0d98a
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-18 20:12:46 +0200

    Create CampaignMember.cs
    Vytvořena class CampaignMember

---
+++ Ghoredin/src/Ghoredin.Domain/Campaigns/CampaignMember.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Domain.Campaigns
+{
+    public class CampaignMember
+    {
+        public Guid id { get; set; }
+        public Guid CampaignId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public CampaignRole Role { get; set; }
+        public Guid? CharacterId { get; set; }
+    }
+}
