2305d2982052427400a19ccee1e8ba04cb95f0b1
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-18 19:43:49 +0200

    Create CharacterDto.cs
    Vytvořene zapomenuté dto pro správu characterů.

---
+++ Ghoredin/src/Ghoredin.Application/Characters/CharacterDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Characters
+{
+    public record CharacterDto(
+        Guid Id,
+        string Name,
+        string GameSystemId,
+        Guid? CampaignId,
+        string? PortraitUrl,
+        Dictionary<string, object> SheetData
+    );
+}
