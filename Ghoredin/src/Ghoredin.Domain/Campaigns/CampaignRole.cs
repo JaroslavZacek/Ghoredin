5873abf25182080a5ea876290b7417a82bfbd567
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-18 20:08:10 +0200

    Create CampaignRole.cs
    Vytvořen enum pro role v dobrodružství.

---
+++ Ghoredin/src/Ghoredin.Domain/Campaigns/CampaignRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Domain.Campaigns
+{
+    public enum CampaignRole
+    {
+        GameMaster,
+        Player
+    }
+}
