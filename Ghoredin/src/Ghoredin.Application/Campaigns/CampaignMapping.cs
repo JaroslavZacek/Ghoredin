fbd3e0592edcd02f8e19e9559c4fa436cc578949
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 00:06:55 +0200

    Create CampaignMapping.cs
    Vytvoření mapování entit a dtos pro dobrodružství

---
+++ Ghoredin/src/Ghoredin.Application/Campaigns/CampaignMapping.cs
@@ -0,0 +1,33 @@
+﻿using Ghoredin.Domain.Campaigns;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Campaigns
+{
+    public static class CampaignMapping
+    {
+        public static CampaignDto ToDto(this Campaign campaign)
+        {
+            return new CampaignDto(
+                campaign.Id,
+                campaign.Name,
+                campaign.GameSystemId,
+                campaign.MaxPlayers,
+                campaign.Members.Count(m => m.Role == CampaignRole.Player),
+                campaign.Members.Select(m => m.ToDto()).ToList()
+            );
+        }
+
+        public static CampaignMemberDto ToDto(this CampaignMember member)
+        {
+            return new CampaignMemberDto(
+                member.Id,
+                member.UserId,
+                member.Role.ToString(),
+                member.CharacterId
+            );
+        }
+    }
+}
