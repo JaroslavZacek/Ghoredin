fff9c80a2f70f799bdf8b095d8b72d5af65c6a1d
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 00:06:26 +0200

    Create CampaignMemberDto.cs
    Vytvoření CampaignMemberDto.cs

---
+++ Ghoredin/src/Ghoredin.Application/Campaigns/CampaignMemberDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Campaigns
+{
+    public record CampaignMemberDto
+    (
+        Guid Id,
+        string UserId,
+        string Role,
+        Guid? CharacterId
+    );
+}
