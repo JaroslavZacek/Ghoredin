4f7221486eeea619858dbb5035373423795b0764
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 00:05:47 +0200

    Create CreateCampaignCommand.cs
    Vytvořen record CreateCampaignComand

---
+++ Ghoredin/src/Ghoredin.Application/Campaigns/CreateCampaignCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Campaigns
+{
+    public record CreateCampaignCommand
+    (
+        string Name,
+        string GameSystemId,
+        int? MaxPlayers
+    );
+}
