7f960309797150a3e7300f8d64e330af250e125e
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 00:04:41 +0200

    Create ICampaignRepository.cs
    Vytvoření repozitáře ICampaignRepository.cs

---
+++ Ghoredin/src/Ghoredin.Application/Campaigns/ICampaignRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ghoredin.Domain.Campaigns;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Campaigns
+{
+    public interface ICampaignRepository
+    {
+        Task<Campaign?> GetByIdAsync(Guid id);
+        Task<List<Campaign>> GetAllAsync();
+        Task<List<Campaign>> GetByMemberAsync(string userId);
+        Task AddAsync(Campaign campaign);
+        Task SaveChangesAsync();
+    }
+}
