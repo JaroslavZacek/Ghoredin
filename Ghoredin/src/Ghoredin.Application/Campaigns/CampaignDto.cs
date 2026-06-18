12128c1f4ac622e27b7eb5f1a883012cb3805f29
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 00:06:09 +0200

    Create CampaignDto.cs
    Vytvořeno CampaignDto.cs

---
+++ Ghoredin/src/Ghoredin.Application/Campaigns/CampaignDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Campaigns
+{
+    public record CampaignDto
+    (
+        Guid Id,
+        string Name,
+        string GameSystemId,
+        int? MaxPlayers,
+        int PlayerCount,
+        List<CampaignMemberDto> Members
+    );
+}
