8787082df7740a90f34b5f9d008f2406b152eebe
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-18 10:33:39 +0200

    Create CharacterRepository.cs
    Vytvořen CharacterRepository s metodamy pro práci s hráčskými postavami

---
+++ Ghoredin/src/Ghoredin.Infrastructure/Persistence/Repositories/CharacterRepository.cs
@@ -0,0 +1,64 @@
+﻿using Ghoredin.Application.Characters;
+using Ghoredin.Domain.Characters;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Infrastructure.Persistence.Repositories
+{
+    public class CharacterRepository : ICharacterRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CharacterRepository(AppDbContext contexr)
+        {
+            _context = contexr;
+        }
+
+        /// <summary>
+        /// Vrátí entitu postavy podle jejího identifikátoru (GUID).
+        /// </summary>
+        /// <param name="id">Identifikátor postavy (GUID).</param>
+        /// <returns>Asynchronní <see cref="Task{Character}"/> vracející nalezenou instanci <see cref="Character"/> nebo <c>null</c>,
+        /// pokud žádná entita s daným id neexistuje.
+        /// </returns>
+        public async Task<Character> GetByIdAsync(Guid id)
+        {
+            return await _context.Characters.FindAsync(id);
+        }
+
+        /// <summary>
+        /// Asynchronně načte všechny entity typu <see cref="Character"/>, které vlastní uživatel se zadaným ID.
+        /// </summary>
+        /// <param name="ownerUserId">Identifikátor vlastníka postav (např. uživatelské Id). Očekává se neprázdný řetězec.</param>
+        /// <returns>Seznam <see cref="Character"/> entit přiřazených zadanému uživateli. Pokud nejsou nalezeny žádné záznamy, metoda vrátí prázdný seznam.</returns>
+        public async Task<List<Character>> GetByOwnerAsync(string ownerUserId)
+        {
+            return await _context.Characters
+                .Where(c => c.OwnerUserId == ownerUserId)
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Asynchronně přidá novou instanci <c>Character</c> do DbSetu <c>Characters</c> v kontextu.
+        /// </summary>
+        /// <param name="character">Objekt <c>Character</c>, který má být přidán. Nesmí být <c>null</c>.</param>
+        /// <returns>Asynchronický <c>Task</c>. Dokončení znamená, že entita byla úspěšně přidána do kontextu.</returns>
+        public async Task AddAsync(Character character)
+        {
+            await _context.Characters.AddAsync(character);
+        }
+
+        /// <summary>
+        /// Asynchronně uloží všechny neuložené změny aktuálního DbContextu do databáze.
+        /// </summary>
+        /// <returns>Asynchronní <see cref="Task"/>, která se dokončí po úspěšném uložení změn.</returns>
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
