31d5502cc1322d461dbeba7d7565f371edac6c7b
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-18 19:53:59 +0200

    Create CharacterMapping.cs
    Vytvořena učební class CharacterMapping.cs, která slouží k mapování entity Character na dto CharacterDto

---
+++ Ghoredin/src/Ghoredin.Application/Characters/CharacterMapping.cs
@@ -0,0 +1,23 @@
+﻿using Ghoredin.Domain.Characters;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Characters
+{
+    public static class CharacterMapping
+    {
+        public static CharacterDto ToDto(this Character character)
+        {
+            return new CharacterDto(
+                character.Id,
+                character.Name,
+                character.GameSystemId,
+                character.CampaignId,
+                character.PortraitUrl,
+                character.SheetData
+            );
+        }
+    }
+}
