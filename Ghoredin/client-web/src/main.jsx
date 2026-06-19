c8459e090f136cf49a49fbd085e7d604887482a2
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 16:44:42 +0200

    Update main.jsx
    Přidán BrowserRouter

--- Ghoredin/client-web/src/main.jsx
+++ Ghoredin/client-web/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './features/auth/AuthContext.jsx'
+
 import App from './App.jsx'
 
 import './index.css'
@@ -8,8 +10,10 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
