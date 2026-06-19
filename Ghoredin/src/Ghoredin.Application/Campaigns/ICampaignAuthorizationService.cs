40ae7a6bca92a4635f61d03b5144951922652d57
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 13:53:11 +0200

    Create ICampaignAuthorizationService.cs
    Vytvořeno rozhraní pro CampaignAuthorizationService.cs

---
+++ Ghoredin/src/Ghoredin.Application/Campaigns/ICampaignAuthorizationService.cs
@@ -0,0 +1,14 @@
+﻿using Ghoredin.Domain.Campaigns;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Campaigns
+{
+    public interface ICampaignAuthorizationService
+    {
+        bool IsGameMaster(Campaign campaign, string userId);
+        bool IsMember(Campaign campaign, string userId);
+    }
+}
