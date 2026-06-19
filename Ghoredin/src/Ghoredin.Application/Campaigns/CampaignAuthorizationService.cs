5fd8ff384ba5bef1f520065da2d91fe5e39c444d
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 13:58:32 +0200

    Create CampaignAuthorizationService.cs
    Vytvořena CampaingAuthorizationService.cs, která má metody pro určení zda je připojený hráč GM nebo jen člen dobrodružství s postavou.

---
+++ Ghoredin/src/Ghoredin.Application/Campaigns/CampaignAuthorizationService.cs
@@ -0,0 +1,21 @@
+﻿using Ghoredin.Domain.Campaigns;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Campaigns
+{
+    public class CampaignAuthorizationService : ICampaignAuthorizationService
+    {
+        public bool IsGameMaster(Campaign campaign, string userId)
+        {
+            return campaign.Members.Any(m => m.UserId == userId && m.Role == CampaignRole.GameMaster);
+        }
+
+        public bool IsMemeber(Campaign campaign, string userId)
+        {
+            return campaign.Members.Any(m => m.UserId == userId);
+        }
+    }
+}
