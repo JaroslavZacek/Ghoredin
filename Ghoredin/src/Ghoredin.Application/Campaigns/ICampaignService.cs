ba16bfbb18c31e6c7879b6ee0f783789717bf9d7
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 11:24:02 +0200

    Create ICampaignService.cs
    Vytvořeno rozhraní pro CampaignService

---
+++ Ghoredin/src/Ghoredin.Application/Campaigns/ICampaignService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Campaigns
+{
+    public interface ICampaignService
+    {
+        Task<CampaignDto> CreateAsync(CreateCampaignCommand command);
+        Task<List<CampaignDto>> GetMyCampaignsAsync();
+        Task<CampaignDto?> GetByIdAsync(Guid id); 
+    }
+}
