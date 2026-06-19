b6f3a1ce5e1bcb727683656a67e6d770416d3826
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 17:06:18 +0200

    Update App.jsx
    Vytvořeno navigační menu, pro přihlášeného uživatele

--- Ghoredin/client-web/src/App.jsx
+++ Ghoredin/client-web/src/App.jsx
@@ -1,7 +1,12 @@
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+
 import { useAuth } from "./features/auth/AuthContext";
 
 import AuthPage from "./features/auth/components/AuthPage";
 import CharacterList from "./features/characters/components/CharacterList";
+import CampaignList from "./features/campaigns/components/CampaignList";
+
+import "./App.css"
 
 function App() {
   const { user, loading, logout} = useAuth();
@@ -19,14 +24,26 @@ function App() {
   }
 
   return (
-    <div style={{ maxWidth: 600, margin: "40px auto", fontFamily: "var(--font-sans)"}}>
-      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: 24}}>
-        <h1 style={{ fontSize: 22, margin: 0 }}>Kronika Ghoredinu</h1>
+    <div className="app">
+      <header className="app_header">
+        <h1 className="app__title">Kronika Ghoredinu</h1>
 
-        <button className="auth-button" onClick={logout}>Odhlásit se</button>
-      </div>
+        <nav className="app__nav">
+          <Link className="app__nav-link">Postavy</Link>
+          <Link className="app__nav-link">Dobrodružství</Link>
 
-      <CharacterList />
+          <button className="auth-button">Odhlásit se</button>
+        </nav>
+      </header>
+
+      <main className="app__main">
+        <Routes>
+          <Route path="/characters" element={<CharacterList />} />
+          <Route path="/campaigns" element={<CampaignList />} />
+          {/* Výchozí adresa -> přesměrování na postavy*/}
+          <Route path="*" element={<Navigate to="/characters" replace />} /> 
+        </Routes>
+      </main>
     </div>
   );
 }
