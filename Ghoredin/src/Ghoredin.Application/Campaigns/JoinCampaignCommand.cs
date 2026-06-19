9d0cde98c94d1063991026049288cf85fff7b136
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 13:48:38 +0200

    Create JoinCampaignCommand.cs
    Vytvořen command pro připojení do dobrodružství. Hráč se může připojit i bez vytvořené postavy, kterou přidá později

---
+++ Ghoredin/src/Ghoredin.Application/Campaigns/JoinCampaignCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Campaigns
+{
+    public record JoinCampaignCommand
+    (
+        Guid CampaignId,
+        Guid? CharacterId
+    );
+}
