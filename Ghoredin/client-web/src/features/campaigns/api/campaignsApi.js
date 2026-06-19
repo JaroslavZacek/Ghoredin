289eed5b4f09ae6eeea19bb6cc5dddf0c409716f
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-19 17:22:11 +0200

    Create campaignsApi.js
    Vytvořeno campaignsApi.js, kde máme metody pro práci s dobrodružstvím. Přidané metody getMyCampaigns, getAvailableCampaigns, createCampaign a joinCampaign

---
+++ Ghoredin/client-web/src/features/campaigns/api/campaignsApi.js
@@ -0,0 +1,13 @@
+import { apiGet, apiPost } from "../../../shared/api/apiClient";
+
+export const getMyCampaigns = () =>
+    apiGet("campaigns");
+
+export const getAvailableCampaigns = () =>
+    apiGet("campaigns/available");
+
+export const createCampaign = (data) =>
+    apiPost("campaigns", data);
+
+export const joinCampaign = (campaignId, characterId = null) =>
+    apiPost(`campaigns/${campaignId}/join`, { characterId });
