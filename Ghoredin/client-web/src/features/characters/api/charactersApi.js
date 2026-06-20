6d03e015c239ffc2a779304f27957f41610f7d6e
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-20 16:32:30 +0200

    Update charactersApi.js
    Přidány nové metody. getCampaignCharacters, která vypíše postavy náležící  dobrodružství. createCharacterInCampaign pro vytvoření postavy v dobrodružství.

--- Ghoredin/client-web/src/features/characters/api/charactersApi.js
+++ Ghoredin/client-web/src/features/characters/api/charactersApi.js
@@ -8,3 +8,9 @@ export const getCharacter = (id) =>
 
 export const createCharacter = (data) => 
     apiPost("characters", data);
+
+export const getCampaignCharacters = (campaignId) =>
+    apiGet(`characters/campaign/${campaignId}`);
+
+export const createCharacterInCampaign = (campaignId, data) =>
+    apiPost(`characters/campaign/${campaignId}`, data);
