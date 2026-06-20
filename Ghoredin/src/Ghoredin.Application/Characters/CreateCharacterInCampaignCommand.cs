7896bd89d7ec274f44217e79006e4dd4d0bd9693
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-20 08:51:09 +0200

    Create CreateCharacterInCampaignCommand.cs
    Vytvořen record CreateCharacterInCampaignComman

---
+++ Ghoredin/src/Ghoredin.Application/Characters/CreateCharacterInCampaignCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Characters
+{
+    public record CreateCharacterInCampaignCommand
+    (
+        Guid CampaignId,
+        string Name,
+        Dictionary<string, object> SheetData
+    );
+
+}
