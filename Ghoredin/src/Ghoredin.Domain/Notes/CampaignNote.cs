1f351ef98737f9a44d0783cbaf6a4bc33bdbbdb2
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-23 01:37:48 +0200

    Create CampaignNote.cs
    Vytvořena CampaignNote, kde si bude GM držet své poznámky k dobrodružství.

---
+++ Ghoredin/src/Ghoredin.Domain/Notes/CampaignNote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Domain.Notes
+{
+    public class CampaignNote
+    {
+        public Guid Id { get; set; }
+        public Guid CampaignId { get; set; }
+        public string AuthorUserId { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+        public string Content {  get; set; } = string.Empty;
+
+        public string? PlayerFacingContent { get; set; }
+
+        public NoteVisibility Visibility { get; set; } = NoteVisibility.GmOnly;
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
