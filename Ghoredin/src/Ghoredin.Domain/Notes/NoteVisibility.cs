6b9e5db4a0e2b509334cac9e994c2717085ed121
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-23 01:21:02 +0200

    Create NoteVisibility.cs
    Vytvořen enum pro uchování hodnot zda je viditelné jen pro GM nebo i umožnuju sdílet s hráči

---
+++ Ghoredin/src/Ghoredin.Domain/Notes/NoteVisibility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Domain.Notes
+{
+    public enum NoteVisibility
+    {
+        GmOnly,
+        SharedWithPlayers
+    }
+}
