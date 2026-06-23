151d29e2c036b19a0ea369471625464a2b073418
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-23 10:08:45 +0200

    Create INoteRepository.cs
    Vytvoření INoteRepository s metodami pro práci s Note

---
+++ Ghoredin/src/Ghoredin.Application/Notes/INoteRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ghoredin.Domain.Notes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Notes
+{
+    public interface INoteRepository
+    {
+        Task<CampaignNote?> GetByIdAsync(Guid id);
+        Task<List<CampaignNote>> GetByCampaignAsync(Guid campaignId);
+        Task AddAsync(CampaignNote note);
+        Task SaveChangesAsync();
+    }
+}
