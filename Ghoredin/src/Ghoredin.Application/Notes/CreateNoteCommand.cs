2b083469dd05897d41a4453a1787a26c2fbe7692
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-23 10:09:10 +0200

    Create CreateNoteCommand.cs
    Nový record pro založení note

---
+++ Ghoredin/src/Ghoredin.Application/Notes/CreateNoteCommand.cs
@@ -0,0 +1,16 @@
+﻿using Ghoredin.Domain.Notes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Notes
+{
+    public record CreateNoteCommand
+    (
+        Guid CamapignId,
+        string Title,
+        string Content,
+        string? PlayerFacingContent,
+        NoteVisibility Visibility
+    );
+}
