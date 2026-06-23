64e005188a55ba462c4cf78df203f64bf53d8ecf
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-23 10:20:36 +0200

    Create NoteMapping.cs
    Vytvoření mapování pro NoteDto

---
+++ Ghoredin/src/Ghoredin.Application/Notes/NoteMapping.cs
@@ -0,0 +1,27 @@
+﻿using Ghoredin.Domain.Notes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Notes
+{
+    public static class NoteMapping
+    {
+        public static NoteDto ToDto(this CampaignNote note, bool isGameMaster)
+        {
+            var content = isGameMaster ? note.Content : string.Empty;
+
+            return new NoteDto(
+                note.Id,
+                note.CampaignId,
+                note.AuthorUserId,
+                note.Title,
+                content,
+                note.PlayerFacingContent,
+                note.Visibility.ToString(),
+                note.CreatedAt,
+                note.UpdatedAt
+            );
+        }
+    }
+}
