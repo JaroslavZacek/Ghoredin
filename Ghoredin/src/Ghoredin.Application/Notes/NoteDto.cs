c50c82a5b2df43a3bbe4eccb568af4f918f01f07
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-23 10:14:57 +0200

    Create NoteDto.cs
    Vytvořeno Dto pro Note

---
+++ Ghoredin/src/Ghoredin.Application/Notes/NoteDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Notes
+{
+    public record NoteDto
+    (
+        Guid Id,
+        Guid CampaignId,
+        string AuthorUserId,
+        string Title,
+        string Content,
+        string? PlayerFacingContent,
+        string Visibility,
+        DateTime CreatedAt,
+        DateTime UpdatedAt
+    );
+}
