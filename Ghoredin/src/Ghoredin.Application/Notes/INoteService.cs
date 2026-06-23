8cebeb61be36c88a55c4b336b9d0bb2d7d4205e2
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-23 10:22:48 +0200

    Create INoteService.cs
    Vytvořeno rozhraní pro NoteServices

---
+++ Ghoredin/src/Ghoredin.Application/Notes/INoteService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Notes
+{
+    public interface INoteService
+    {
+        Task<NoteDto> CreateAsync(CreateNoteCommand command);
+        Task<NoteDto> UpdateAsync(UpdateNoteCommand command);
+        Task<List<NoteDto>> GetCampaignNotesAsync(Guid campaignId);
+    }
+}
