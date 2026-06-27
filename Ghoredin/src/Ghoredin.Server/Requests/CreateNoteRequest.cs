721d81be71f2289e6ca6b9e874a33d8e80cf887d
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-28 00:01:57 +0200

    Create CreateNoteRequest.cs
    Vytvořen request pro vytvoření poznámky

---
+++ Ghoredin/src/Ghoredin.Server/Requests/CreateNoteRequest.cs
@@ -0,0 +1,13 @@
+﻿using Ghoredin.Domain.Notes;
+
+namespace Ghoredin.Server.Requests
+{
+    public record CreateNoteRequest
+    (
+        Guid CampaignId,
+        string Title,
+        string Content,
+        string? PlayerFacingContent,
+        NoteVisibility Visibility
+    );
+}
