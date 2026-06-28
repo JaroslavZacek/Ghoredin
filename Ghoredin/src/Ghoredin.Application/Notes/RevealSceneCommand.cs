6b32e2fa45fe6d90b310323e1b506fdf780f8919
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-28 12:55:20 +0200

    Create RevealSceneCommand.cs
    Vytvořen record pro uložení komu je scéna zobrazena

---
+++ Ghoredin/src/Ghoredin.Application/Notes/RevealSceneCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Notes
+{
+    public record RevealSceneCommand
+    (
+        Guid CampaignId,
+        Guid NoteId,
+        List<string> TargetUserIds
+    );
+}
