7c96ebb18a2cacb202a75588566270a81db42219
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-06-28 12:52:27 +0200

    Add Migration and update DB

---
+++ Ghoredin/src/Ghoredin.Infrastructure/Persistence/Migrations/20260628104713_AddCurrentSceneToMember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ghoredin.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrentSceneToMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CurrentSceneNoteId",
+                table: "CampaignMembers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentSceneNoteId",
+                table: "CampaignMembers");
+        }
+    }
+}
