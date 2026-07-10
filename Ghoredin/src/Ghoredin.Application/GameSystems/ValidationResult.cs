4b6655260c9fd86c5411880a4431d7f4586d2f09
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-07-10 23:46:54 +0200

    Create ValidationResult.cs
    Vytvoření validace pro postavy

---
+++ Ghoredin/src/Ghoredin.Application/GameSystems/ValidationResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.GameSystems
+{
+    public record ValidationResult(bool IsValid, List<string> Errors)
+    {
+        public static ValidationResult Success() => new ValidationResult(true, new List<string>());
+        public static ValidationResult Failure(params string[] errors) => new(false, errors.ToList());
+    }
+}
