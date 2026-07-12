5cb665d2703f42ff968c7819d5634b446b0c93f7
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-07-12 17:44:33 +0200

    Create GameSystemRegistry.cs
    Registrace nových herních systémů s metodami pro přidání nového systému, jestli daný systém již v systému existuje a výpisu všech herních systémů v registru.

---
+++ Ghoredin/src/Ghoredin.Infrastructure/GameSystems/GameSystemRegistry.cs
@@ -0,0 +1,32 @@
+﻿using Ghoredin.Application.GameSystems;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Infrastructure.GameSystems
+{
+    public class GameSystemRegistry : IGameSystemRegistry
+    {
+        private readonly Dictionary<string, IGameSystem> _systems;
+
+        public GameSystemRegistry(IEnumerable<IGameSystem> systems)
+        {
+            _systems = systems.ToDictionary(s => s.Id, s => s);
+        }
+
+        public IGameSystem Get(string gameSystemId)
+        {
+            if (_systems.TryGetValue(gameSystemId, out var system))
+                return system;
+
+            throw new InvalidOperationException($"Neznámý herní systém: {gameSystemId}");
+        }
+
+        public bool Exists(string gameSystemId)
+            => _systems.ContainsKey(gameSystemId);
+
+        public IReadOnlyList<IGameSystem> GetAll()
+            => _systems.Values.ToList();
+    }
+}
