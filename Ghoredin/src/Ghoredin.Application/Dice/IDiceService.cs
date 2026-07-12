660782f91c575448ef17d4d610f7e1fe5ee59e45
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-07-12 23:25:57 +0200

    Create IDiceService.cs
    Vytvořeno rozhrani pro DiceService

---
+++ Ghoredin/src/Ghoredin.Application/Dice/IDiceService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Dice
+{
+    public interface IDiceService
+    {
+        int Roll(DiceType dice);
+
+        int[] RollMany(DiceType dice, int count);
+    }
+}
