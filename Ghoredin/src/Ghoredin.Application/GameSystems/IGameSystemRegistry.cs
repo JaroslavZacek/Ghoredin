7d6557fa4b739866006f7e5bb78ee55969513c6a
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-07-12 10:15:05 +0200

    Create IGameSystemRegistry.cs
    Vytvoření registru systému pro postavy

---
+++ Ghoredin/src/Ghoredin.Application/GameSystems/IGameSystemRegistry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.GameSystems
+{
+    public interface IGameSystemRegistry
+    {
+        IGameSystem Get(string gameSystemId);
+
+        bool Exists(string gameSystemId);
+
+        IReadOnlyList<IGameSystem> GetAll();
+    }
+}
