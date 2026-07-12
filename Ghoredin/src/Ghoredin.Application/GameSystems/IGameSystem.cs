db6a5189db292e8eacf9c3e939a1a5c49d1c8bec
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-07-12 10:11:20 +0200

    Create IGameSystem.cs
    Vytvořeno rozhraní pro GameSystem

---
+++ Ghoredin/src/Ghoredin.Application/GameSystems/IGameSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.GameSystems
+{
+    public interface IGameSystem
+    {
+        string Id { get; }
+
+        string DisplayName { get; }
+
+        bool SupportsLeveling { get; }
+
+        Dictionary<string, object> CreateDefaultSheet();
+
+        ValidationResult ValidateSheet(Dictionary<string, object> sheetData);
+    }
+}
