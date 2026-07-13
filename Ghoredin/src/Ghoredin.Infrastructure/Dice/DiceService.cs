c140fa3b29cb4002b8e09797df28351c0d1f43ad
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-07-13 14:54:44 +0200

    Create DiceService.cs
    Vytvořena třída DiceService. Přidány metody roll pro jednotlivý hod a RollMany pro více hodů, které poté vrátí

---
+++ Ghoredin/src/Ghoredin.Infrastructure/Dice/DiceService.cs
@@ -0,0 +1,30 @@
+﻿using Ghoredin.Application.Dice;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Infrastructure.Dice
+{
+    public class DiceService : IDiceService
+    {
+        private readonly Random _random = new();
+
+        public int Roll(DiceType dice)
+        {
+            int sides = (int)dice;
+
+            return _random.Next(1, sides + 1);
+        }
+
+        public int[] RollMany(DiceType dice, int count)
+        {
+            var results = new int[count];
+
+            for (int i = 0; i < count; i++)
+                results[i] = Roll(dice);
+
+            return results;
+        }
+    }
+}
