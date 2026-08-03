6bee4a5e3e1cd7c040d9ebbace339c5b053b9608
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-03 22:01:42 +0200

    Update characterApi.js
    Přidány endpointy pro tvorbu postavy s pravidlem nahazování postavy přes hod kostkou.

--- Ghoredin/client-web/src/features/characters/api/charactersApi.js
+++ Ghoredin/client-web/src/features/characters/api/charactersApi.js
@@ -11,3 +11,12 @@ export const getCampaignCharacters = (campaignId) =>
 
 export const createCharacterInCampaign = (campaignId, data) =>
     apiPost(`characters/campaign/${campaignId}`, data);
+
+export const startRolledCharacter = (campaignId, name) =>
+    apiPost(`characters/campaign/${campaignId}/start-rolled`, { name });
+
+export const rollAbility = (characterId, abilityName) =>
+    apiPost(`characters/${characterId}/roll-ability`, { abilityName });
+
+export const completeRolledCharacter = (characterId) =>
+    apiPost(`characters/${characterId}/complete-rolled`, {});
