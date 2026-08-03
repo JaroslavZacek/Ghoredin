16ce084cdf54ea2a8c8e4d5fea2a8a45ff08a6ac
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-03 11:52:47 +0200

    Update DiceService.cs
    Vytvořeno summary komentáře pro DiceService

--- Ghoredin/src/Ghoredin.Infrastructure/Dice/DiceService.cs
+++ Ghoredin/src/Ghoredin.Infrastructure/Dice/DiceService.cs
@@ -10,6 +10,11 @@ namespace Ghoredin.Infrastructure.Dice
     {
         private readonly Random _random = new();
 
+        /// <summary>
+        /// Hození kostkou s daným počtem stran (dice)
+        /// </summary>
+        /// <param name="dice">Počet stran kostky</param>
+        /// <returns>Náhodné číslo mezi 1 a počtem stran kostky</returns>
         public int Roll(DiceType dice)
         {
             int sides = (int)dice;
@@ -17,6 +22,12 @@ namespace Ghoredin.Infrastructure.Dice
             return _random.Next(1, sides + 1);
         }
 
+        /// <summary>
+        /// Hození kostkou s daným počtem stran (dice) vícekrát (count)
+        /// </summary>
+        /// <param name="dice">Počet stran kostky</param>
+        /// <param name="count">Počet kostek, které se mají hodit</param>
+        /// <returns>Pole náhodných čísel</returns>
         public int[] RollMany(DiceType dice, int count)
         {
             var results = new int[count];
