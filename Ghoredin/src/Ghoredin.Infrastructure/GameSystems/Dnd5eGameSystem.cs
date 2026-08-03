2553e8ebdd37fe5a22a5b2fff956dcf075c4e015
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-03 14:40:52 +0200

    Update Dnd5eGameSystem.cs
    Přidáno validování pro tvorbu postavy.

--- Ghoredin/src/Ghoredin.Infrastructure/GameSystems/Dnd5eGameSystem.cs
+++ Ghoredin/src/Ghoredin.Infrastructure/GameSystems/Dnd5eGameSystem.cs
@@ -104,6 +104,93 @@ namespace Ghoredin.Infrastructure.GameSystems
                 : ValidationResult.Failure(errors.ToArray());
         }
 
+        // Validace point buy systému tvorby postavy
+        public ValidationResult ValidatePointBuy(Dictionary<string, object> abilities)
+        {
+            var errors = new List<string>();
+
+            int totalCost = 0;
+
+            foreach (var name in AbilityNames)
+            {
+                var val = ToInt(abilities.GetValueOrDefault(name));
+
+                if (val is null)
+                {
+                    errors.Add($"Chybí atribut '{name}'.");
+                }
+
+                if (!PointBuyCost.ContainsKey(val.Value))
+                {
+                    errors.Add($"Atribut '{name}' musí být 8-15 (je {val}.");
+                    continue;
+                }
+
+                totalCost += PointBuyCost[val.Value];
+            }
+
+            if (totalCost > PointBuyBudget)
+                errors.Add($"Překročen rozpočet bodů ({totalCost}/{PointBuyBudget}).");
+
+            return errors.Count == 0
+                ? ValidationResult.Success()
+                : ValidationResult.Failure(errors.ToArray());
+        }
+
+        public ValidationResult ValidateStandardArray(Dictionary<string, object> abilities)
+        {
+            var errors = new List<string>();
+            var used = new List<int>();
+
+            foreach (var name in AbilityNames)
+            {
+                var val = ToInt(abilities.GetValueOrDefault(name));
+
+                if (val is null)
+                {
+                    errors.Add($"Chybí atribut '{name}'.");
+                    continue;
+                }
+                used.Add(val.Value);
+            }
+
+            // Porovnání seznamu použitých hodnot s očekávaným standardním polem
+            if (errors.Count == 0)
+            {
+                var sortedUsed = used.OrderBy(x => x).ToArray();
+                var sortedArray = StandardArray.OrderBy(x => x).ToArray();
+
+                if (!sortedUsed.SequenceEqual(sortedArray))
+                    errors.Add("Musíš použít přesně sadu 15, 14, 13, 12, 10, 8 (každé jednou).");
+            }
+
+            return errors.Count == 0
+                ? ValidationResult.Success()
+                : ValidationResult.Failure(errors.ToArray());
+        }
+
+        #endregion
+
+        #region Tvorba postavy
+
+        // Point buy: kolik bodů stojí která hodnota atributu
+        private static readonly Dictionary<int, int> PointBuyCost = new()
+        {
+            [8] = 0,
+            [9] = 1,
+            [10] = 2,
+            [11] = 3,
+            [12] = 4,
+            [13] = 5,
+            [14] = 7,
+            [15] = 9
+        };
+
+        private const int PointBuyBudget = 27;
+
+        // Standard array: pevná sada hodnot atributů
+        private static readonly int[] StandardArray = { 15, 14, 13, 12, 10, 8 };
+
         #endregion
 
         #region pomocné metody pro práci s JSON hodnotami
