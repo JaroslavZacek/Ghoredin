bec85bd3214df891f0a33ed555ae5306e16dc4dc
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-03 13:28:51 +0200

    Create CharacterCreationMethod.cs
    Vytvořen enum pro rozlišování jakým způsobem se bude vytvářet postava v daném dobrodružství.

---
+++ Ghoredin/src/Ghoredin.Application/GameSystems/CharacterCreationMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.GameSystems
+{
+    public enum CharacterCreationMethod
+    {
+        PointBuy,
+        StandardArray,
+        Roll
+    }
+}
