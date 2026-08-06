fe68a975db93fc0c4cfbfa05783b95db02d40d63
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-06 12:34:51 +0200

    Create 20260806101457_AddIsActiveToCampaignMember.cs
    Vytvořena migrace

---
+++ Ghoredin/src/Ghoredin.Infrastructure/Persistence/Migrations/20260806101457_AddIsActiveToCampaignMember.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ghoredin.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsActiveToCampaignMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "CampaignMembers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "CampaignMembers");
+        }
+    }
+}
