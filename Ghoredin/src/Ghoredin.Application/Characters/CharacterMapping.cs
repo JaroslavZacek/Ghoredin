2fe60f393f7d382e52c9eb3099c6dd595e2bf2e4
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-06 07:15:29 +0200

    Update CharacterMapping.cs
    Přidán campaignName pro odesílání i pojmenování dobrodružství, kam postava patří

--- Ghoredin/src/Ghoredin.Application/Characters/CharacterMapping.cs
+++ Ghoredin/src/Ghoredin.Application/Characters/CharacterMapping.cs
@@ -8,13 +8,14 @@ namespace Ghoredin.Application.Characters
 {
     public static class CharacterMapping
     {
-        public static CharacterDto ToDto(this Character character)
+        public static CharacterDto ToDto(this Character character, string? campaignName)
         {
             return new CharacterDto(
                 character.Id,
                 character.Name,
                 character.GameSystemId,
                 character.CampaignId,
+                campaignName,
                 character.PortraitUrl,
                 character.SheetData
             );
