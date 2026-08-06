a9b18a78cae22b38c0aa52d5efdf84b492fc1d2a
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-06 10:21:57 +0200

    Update CampaignsController.cs
    Vytvoření crud operace pro mazání dobrodružství

--- Ghoredin/src/Ghoredin.Server/Controllers/CampaignsController.cs
+++ Ghoredin/src/Ghoredin.Server/Controllers/CampaignsController.cs
@@ -77,5 +77,24 @@ namespace Ghoredin.Server.Controllers
         }
 
         #endregion
+
+        #region Delete
+
+        [HttpDelete("{id:guid}")] 
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await _campaignService.DeleteAsync(id);
+
+                return NoContent();
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new {error = ex.Message});
+            }
+        }
+
+        #endregion
     }
 }
