ea31f1afb8d06612073a33e8e9551edef1a72ae6
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-09 20:20:57 +0200

    Create IChatNotifier.cs
    Vytvořen IChatNotifier pro komunikaci se SignalR

---
+++ Ghoredin/src/Ghoredin.Application/Chat/IChatNotifier.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Chat
+{
+    public interface IChatNotifier
+    {
+        Task NotifyMessageAsync(ChatMessageDto message, string authorUserId, string? whisperToUserId);
+    }
+}
