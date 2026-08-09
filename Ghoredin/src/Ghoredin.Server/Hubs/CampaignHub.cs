5af5807b8b47852acbcc63b79504a6b37aab4354
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-09 10:25:37 +0200

    Create CampaignHub.cs
    Vytvoření hubu pro připojení k dobrodružství.

---
+++ Ghoredin/src/Ghoredin.Server/Hubs/CampaignHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Ghoredin.Server.Hubs
+{
+    [Authorize]
+    public class CampaignHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task JoinCampaignGroup(string campaignId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, GroupName(campaignId));
+        }
+
+        public async Task LeaveCampaignGroup(string campaignId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, GroupName(campaignId));
+        }
+
+        private static string GroupName(string campaignId) => $"campaign-{campaignId}";
+    }
+}
