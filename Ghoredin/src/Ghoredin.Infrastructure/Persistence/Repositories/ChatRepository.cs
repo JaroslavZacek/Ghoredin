0c35fa886af182dd1d02f903c05947421a8ea4fd
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-09 19:34:52 +0200

    Create ChatRepository.cs
    Vytvořen ChatRepository

---
+++ Ghoredin/src/Ghoredin.Infrastructure/Persistence/Repositories/ChatRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Ghoredin.Application.Chat;
+using Ghoredin.Domain.Chat;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Ghoredin.Infrastructure.Persistence.Repositories
+{
+    public class ChatRepository : IChatRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ChatRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ChatMessage>> GetByCampaignAsync(Guid campaignId, int take = 50)
+        {
+            var messages = await _context.ChatMessages
+                .Where(m => m.CampaignId == campaignId)
+                .OrderByDescending(m => m.CreatedAt)
+                .Take(take)
+                .ToListAsync();
+
+            messages.Reverse();
+
+            return messages;
+        }
+
+        public async Task AddAsync(ChatMessage message)
+        {
+            await _context.ChatMessages.AddAsync(message);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
