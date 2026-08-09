0dd9ab1dcfc9945b89b26691c9343ef8a5ad4036
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-09 13:20:33 +0200

    Create IChatRepository.cs
    Vytvořeno rozhraní pro chat repository

---
+++ Ghoredin/src/Ghoredin.Application/Chat/IChatRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Ghoredin.Domain.Chat;
+
+namespace Ghoredin.Application.Chat
+{
+    public interface IChatRepository
+    {
+        Task<List<ChatMessage>> GetByCampaignAsync(Guid campaignId, int take = 50);
+        Task AddAsync(ChatMessage message);
+        Task SaveChangesAsync();
+    }
+}
