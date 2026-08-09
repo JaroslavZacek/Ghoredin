7fce7c9070f981459665eefb0d06af65a84f0acb
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-09 13:22:17 +0200

    Create SendMessageCommand.cs
    Vytvořen command pro posílání zpráv

---
+++ Ghoredin/src/Ghoredin.Application/Chat/SendMessageCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Chat
+{
+    public record SendMessageCommand(
+        Guid CampaignId,
+        string Content,
+        string? WhisperToUserId
+    );
+}
