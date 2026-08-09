881d5fc2b0e9f743a0aa43d9fada3466fc3edda4
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-09 13:29:46 +0200

    Create ChatMapping.cs
    Vytvořeno mapování pro message dto

---
+++ Ghoredin/src/Ghoredin.Application/Chat/ChatMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Ghoredin.Domain.Chat;
+
+namespace Ghoredin.Application.Chat
+{
+    public static class ChatMapping
+    {
+        public static ChatMessageDto ToDto(this ChatMessage message)
+        {
+            return new ChatMessageDto(
+                message.Id,
+                message.CampaignId,
+                message.AuthorUserId,
+                message.Content,
+                message.WhisperToUserId is not null,
+                message.CreateAt
+                );
+        }
+    }
+}
