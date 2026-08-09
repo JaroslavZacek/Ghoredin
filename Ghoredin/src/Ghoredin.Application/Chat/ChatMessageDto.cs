c779da78581fef9923962d584bd66ffe407680f3
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-09 13:24:22 +0200

    Create ChatMessageDto.cs
    Vytvořena dto pro chat message

---
+++ Ghoredin/src/Ghoredin.Application/Chat/ChatMessageDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Chat
+{
+    public record ChatMessageDto(
+        Guid Id,
+        Guid CampaignId,
+        string AuthorUserId,
+        string Content,
+        bool IsWhisper,
+        DateTime CreateAt
+    );
+}
