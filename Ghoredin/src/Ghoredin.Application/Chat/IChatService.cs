464a61ed78d995c52f5fd4fede10434bc0d49607
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-09 13:32:32 +0200

    Create IChatService.cs
    Vytvořeno rozhraní pro chat services

---
+++ Ghoredin/src/Ghoredin.Application/Chat/IChatService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Chat
+{
+    public interface IChatService
+    {
+        Task<ChatMessageDto> SendMessageAsync(SendMessageCommand command);
+        Task<List<ChatMessageDto>> GetVisibleHistoryAsync(Guid campaignId);
+    }
+}
