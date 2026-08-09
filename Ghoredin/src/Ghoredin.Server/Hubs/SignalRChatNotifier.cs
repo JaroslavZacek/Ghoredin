47de370c52532c831017daa9b83ea170a6a0dda3
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-09 20:48:29 +0200

    Create SignalRChatNotifier.cs
    Vytvořen SignalRChatNotifier.

---
+++ Ghoredin/src/Ghoredin.Server/Hubs/SignalRChatNotifier.cs
@@ -0,0 +1,32 @@
+﻿using Ghoredin.Application.Chat;
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace Ghoredin.Server.Hubs
+{
+    public class SignalRChatNotifier : IChatNotifier
+    {
+        private readonly IHubContext<CampaignHub> _hubContext;
+
+        public SignalRChatNotifier(IHubContext<CampaignHub> hubContext>)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task NotifyMessageAsync(ChatMessageDto message, string authorUserId, string? whisperToUserId)
+        {
+            if (whisperToUserId is null)
+            {
+                await _hubContext.Clients
+                    .Group($"campaign-{message.CampaignId}")
+                    .SendAsync("ReceiveMessage", message);
+            }
+            else
+            {
+                await _hubContext.Clients
+                    .Users(new[] { authorUserId, whisperToUserId })
+                    .SendAsync("ReceiveMessage", message);
+            }
+        }
+    }
+}
