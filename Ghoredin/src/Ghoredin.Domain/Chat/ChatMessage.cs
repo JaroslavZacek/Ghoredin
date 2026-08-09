895880deba38cfe4006f4a18b63527f482962bd8
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-09 13:16:32 +0200

    Create ChatMessage.cs
    Vytvořena entita ChatMessage

---
+++ Ghoredin/src/Ghoredin.Domain/Chat/ChatMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Domain.Chat
+{
+    public class ChatMessage
+    {
+        public Guid Id { get; set; }
+        public Guid CampaignId { get; set; }
+        public string AuthorUserId { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+        public string? WhisperToUserId { get; set; }
+
+        public DateTime CreateAt { get; set; }
+    }
+}
