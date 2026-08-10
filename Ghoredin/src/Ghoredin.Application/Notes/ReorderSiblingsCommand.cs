12c211fb069bbee0d0158d5117655bac8c4569c5
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-10 02:22:29 +0200

    Create ReorderSiblingsCommand.cs
    Vytvořen ReorderSublingsCommand, který pošle pořadí sourozenců pod daným rodičem

---
+++ Ghoredin/src/Ghoredin.Application/Notes/ReorderSiblingsCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Notes
+{
+    public record ReorderSiblingsCommand(
+        Guid CampaignId,
+        Guid? ParentNoteId,
+        List<Guid> OrderNoteIds);
+}
