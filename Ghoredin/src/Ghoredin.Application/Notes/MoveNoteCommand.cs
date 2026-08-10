c31d8f75451dd906e8e12aff5afa2a7cc7bc3908
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-10 02:19:59 +0200

    Create MoveNoteCommand.cs
    Vytvořen command pro přesouvání mezi rodiči nebo na vyšší úroveň

---
+++ Ghoredin/src/Ghoredin.Application/Notes/MoveNoteCommand.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Notes
+{
+    public record MoveNoteCommand(Guid NoteId, Guid? NewParentNoteId);
+}
