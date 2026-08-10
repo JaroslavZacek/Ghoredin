a29d5ff4dd4349519ea3050f7fc3976309b9ce74
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-10 02:10:54 +0200

    Add new Migration
    Přidána migrace pro note

---
+++ Ghoredin/src/Ghoredin.Infrastructure/Persistence/Migrations/20260810000946_AddNoteTreeAndOrder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ghoredin.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNoteTreeAndOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParentNoteId",
+                table: "CampaignNotes",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SortOrder",
+                table: "CampaignNotes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentNoteId",
+                table: "CampaignNotes");
+
+            migrationBuilder.DropColumn(
+                name: "SortOrder",
+                table: "CampaignNotes");
+        }
+    }
+}
