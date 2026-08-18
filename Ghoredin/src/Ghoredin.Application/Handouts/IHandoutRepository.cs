87d3547bc5bd5e5beb46a34a6856f98ee6562382
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-18 13:46:08 +0200

    Create IHandoutRepository.cs
    Vytvořeno rozhraní pro handout repository

---
+++ Ghoredin/src/Ghoredin.Application/Handouts/IHandoutRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Ghoredin.Domain.Notes
+
+namespace Ghoredin.Application.Handouts
+{
+    public interface IHandoutRepository
+    {
+        Task<Handout?> GetByIdAsync(Guid id);
+        Task<List<Handout>> GetByCampaignAsync(Guid campaignId);
+        Task AddAsync(Handout handout);
+        Task DeleteAsync(Handout handout);
+        Task SaveChangesAsync();
+    }
+}
