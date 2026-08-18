8e5f61d0b1d4def1fe069ee01dc843a8b4efc03a
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-18 13:37:13 +0200

    Create Handout.cs
    Vytvořena nová entita

---
+++ Ghoredin/src/Ghoredin.Domain/Notes/Handout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Domain.Notes
+{
+    public class Handout
+    {
+        public Guid Id { get; set; }
+        public Guid CamapaignId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content {  get; set; } = string.Empty;
+        public HandoutShareMode ShareMode { get; set; }
+        public bool IsShared { get; set; }
+        public string? SnapshotContent { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
