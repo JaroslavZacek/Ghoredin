96294a4eebae0304e561c12b19b2cb2ef00c4b84
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-18 13:49:43 +0200

    Create CreateHandoutCommand.cs
    Vytvořen nový record pro vytvoření handoutu

---
+++ Ghoredin/src/Ghoredin.Application/Handouts/CreateHandoutCommand.cs
@@ -0,0 +1,14 @@
+﻿using Ghoredin.Domain.Notes;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Handouts
+{
+    public record CreateHandoutCommand(
+        Guid CampaignId,
+        string Title,
+        string Content,
+        HandoutShareMode ShareMode);
+}
