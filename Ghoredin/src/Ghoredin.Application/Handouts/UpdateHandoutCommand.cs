4be59d00467d045e72a4bd8106a20be9746f9cb1
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-18 13:50:05 +0200

    Create UpdateHandoutCommand.cs
    Vytvořen nový record pro úpravu handoutu

---
+++ Ghoredin/src/Ghoredin.Application/Handouts/UpdateHandoutCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Handouts
+{
+    public record UpdateHandoutCommand(
+        Guid Id,
+        string Title,
+        string Content);
+}
