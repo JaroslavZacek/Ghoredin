6d9142b3af6243f94fa8d9ac79c80eba987d1a46
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-27 10:20:16 +0200

    Create HandoutDto.cs
    Přidána HandoutDto

---
+++ Ghoredin/src/Ghoredin.Application/Handouts/HandoutDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Handouts
+{
+    public record HandoutDto(
+        Guid Id,
+        Guid CampaignId,
+        string Title,
+        string Content,
+        string ShareMode,
+        bool IsShared,
+        DateTime UpdatedAt);
+}
