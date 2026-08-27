78e8f10f9ef19e2abc7b7125cb8a7426f01919be
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-27 10:47:51 +0200

    Create HandoutMapping.cs
    Vytvořeno mapování pro handoutDto

---
+++ Ghoredin/src/Ghoredin.Application/Handouts/HandoutMapping.cs
@@ -0,0 +1,29 @@
+﻿using Ghoredin.Domain.Handouts;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Handouts
+{
+    public static class HandoutMapping
+    {
+        public static HandoutDto ToDto(this Handout handout, bool isGameMaster)
+        {
+            var content = isGameMaster
+                ? handout.Content
+                : (handout.ShareMode == HandoutShareMode.Snapshot
+                    ? handout.SnapshotContent ?? string.Empty
+                    : handout.Content);
+
+            return new HandoutDto(
+                handout.Id,
+                handout.CamapaignId,
+                handout.Title,
+                content,
+                handout.ShareMode.ToString(),
+                handout.IsShared,
+                handout.UpdatedAt);
+        }
+    }
+}
