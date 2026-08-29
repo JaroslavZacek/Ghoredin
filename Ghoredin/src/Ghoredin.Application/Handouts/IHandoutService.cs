8154b5ce8ff8ff77ec28908145f777e69a5a903a
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-08-29 20:22:41 +0200

    Create IHandoutService.cs
    Vytvořeno rozhraní pro HandoutService

---
+++ Ghoredin/src/Ghoredin.Application/Handouts/IHandoutService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Handouts
+{
+    public interface IHandoutService
+    {
+        Task<HandoutDto> CreateAsync(CreateHandoutCommand command);
+        Task<HandoutDto> UpdateAsync(UpdateHandoutCommand command);
+        Task ShareAsync(Guid handoutId);
+        Task UnshareAsync(Guid handoutId);
+        Task DeleteAsync(Guid handoutId);
+        Task<List<HandoutDto>> GetVisibleForCampaignAsync(Guid campaignId);
+    }
+}
