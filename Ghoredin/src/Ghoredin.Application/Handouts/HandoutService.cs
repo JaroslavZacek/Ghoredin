66f14ea9d00b009f7bc786583d1dbd91a1bfc94a
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-02 14:58:44 +0200

    Create HandoutService.cs
    Vytvořena HandoutService.cs a přidána metoda CreateAsync na vytvoření listiny

---
+++ Ghoredin/src/Ghoredin.Application/Handouts/HandoutService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Ghoredin.Application.Campaigns;
+using Ghoredin.Application.Users;
+using Ghoredin.Domain.Handouts;
+
+namespace Ghoredin.Application.Handouts
+{
+    public class HandoutService : IHandoutService
+    {
+        private readonly IHandoutRepository _handoutRepository;
+        private readonly ICampaignRepository _campaignRepository;
+        private readonly ICampaignAuthorizationService _campaignAuthorizationService;
+        private readonly ICurrentUserService _currentUserService;
+
+        public HandoutService(
+            IHandoutRepository handoutRepository,
+            ICampaignRepository campaignRepository,
+            ICampaignAuthorizationService campaignAuthorizationService,
+            ICurrentUserService currentUserService)
+        {
+            _handoutRepository = handoutRepository;
+            _campaignRepository = campaignRepository;
+            _campaignAuthorizationService = campaignAuthorizationService;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<HandoutDto> CreateAsync(CreateHandoutCommand command)
+        {
+            var userId = _currentUserService.UserId
+                ?? throw new InvalidOperationException("Není přihlášený uživatel.");
+
+            var campaign = await _campaignRepository.GetByIdAsync(command.CampaignId)
+                ?? throw new InvalidOperationException("Dobrodružství neexistuje.");
+
+            if (!_campaignAuthorizationService.IsGameMaster(campaign, userId))
+                throw new InvalidOperationException("Jen PJ může vytvářet listiny.");
+
+            var handout = new Handout
+            {
+                Id = Guid.NewGuid(),
+                CampaignId = command.CampaignId,
+                Title = command.Title,
+                Content = command.Content,
+                ShareMode = command.ShareMode,
+                IsShared = false,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            await _handoutRepository.AddAsync(handout);
+            await _handoutRepository.SaveChangesAsync();
+
+            return handout.ToDto(isGameMaster: true);
+        }
+}
