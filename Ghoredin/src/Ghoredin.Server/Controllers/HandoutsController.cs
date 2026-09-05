76df78e3a61a0da14e045031ff16d9ee95f0e47a
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-05 17:40:25 +0200

    Create HandoutsController.cs
    Vytvořen kontroler pro listiny a přidána crud metoda pro získání listin pro určité dobrodružství.

---
+++ Ghoredin/src/Ghoredin.Server/Controllers/HandoutsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Ghoredin.Application.Handouts;
+
+namespace Ghoredin.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class HandoutsController : Controller
+    {
+        private readonly IHandoutService _handoutService;
+
+        public HandoutsController(IHandoutService handoutService)
+        {
+            _handoutService = handoutService;
+        }
+
+        #region Get
+
+        [HttpGet("campaign/{campaignId:guid}")]
+        public async Task<IActionResult> GetForCampaign(Guid campaignId)
+        {
+            try
+            {
+                var handouts = await _handoutService.GetVisibleForCampaignAsync(campaignId);
+
+                return Ok(handouts);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+
+        #endregion
+    }
+}
