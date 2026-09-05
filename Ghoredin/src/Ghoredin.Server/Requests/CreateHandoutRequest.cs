6b592a3fec5f846746293d27785af12eec817ee2
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-05 17:29:09 +0200

    Create CreateHandoutRequest.cs
    Vytvořen request pro vytvoření listiny

---
+++ Ghoredin/src/Ghoredin.Server/Requests/CreateHandoutRequest.cs
@@ -0,0 +1,11 @@
+﻿using Ghoredin.Domain.Handouts;
+
+namespace Ghoredin.Server.Requests
+{
+    public record CreateHandoutRequest(
+        Guid CampaignId,
+        string Title,
+        string Content,
+        HandoutContentType ContentType,
+        HandoutShareMode ShareMode);
+}
