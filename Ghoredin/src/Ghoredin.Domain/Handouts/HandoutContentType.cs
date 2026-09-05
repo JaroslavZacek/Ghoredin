905f2f0ab56170362ac78694ba0f98c5bf620d1f
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-05 13:02:15 +0200

    Create HandoutContentType.cs
    Vytvořen enum pro rozlišení zda je listina text nebo nějaký obrázek. Obrázek odkazuje na url, kde je uložen

---
+++ Ghoredin/src/Ghoredin.Domain/Handouts/HandoutContentType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Domain.Handouts
+{
+    public enum HandoutContentType
+    {
+        Text,
+        ImageUrl
+    }
+}
