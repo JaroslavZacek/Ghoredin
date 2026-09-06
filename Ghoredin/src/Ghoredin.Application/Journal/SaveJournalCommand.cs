45dac188d4b47d4a92cf0282957f00b98f0a5c8b
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-06 22:37:05 +0200

    Create SaveJournalCommand.cs
    Vytvořen command pro uložení deníku

---
+++ Ghoredin/src/Ghoredin.Application/Journal/SaveJournalCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Journal
+{
+    public record SaveJournalCommand(
+        Guid CampaignId,
+        string Content);
+}
