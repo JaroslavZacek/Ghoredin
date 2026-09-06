182a7198c24fff776ab47c74579b1fa599ea8394
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-06 22:43:14 +0200

    Create JournalMapping.cs
    Vytvořeno mapování pro deník

---
+++ Ghoredin/src/Ghoredin.Application/Journal/JournalMapping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Ghoredin.Domain.Journal;
+
+namespace Ghoredin.Application.Journal
+{
+    public static class JournalMapping
+    {
+        public static JournalEntryDto ToDto(this JournalEntry entry)
+        {
+            return new JournalEntryDto(
+                entry.Id,
+                entry.CampaignId,
+                entry.Content,
+                entry.UpdatedAt);
+        }
+    }
+}
