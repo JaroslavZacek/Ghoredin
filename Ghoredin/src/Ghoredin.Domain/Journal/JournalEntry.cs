d3e5bf2e405535e71c9350a2f4beef46eeb8fe72
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-06 22:31:55 +0200

    Create JournalEntry.cs
    Vytvořena entita deníku, pro zápisky postav.

---
+++ Ghoredin/src/Ghoredin.Domain/Journal/JournalEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Domain.Journal
+{
+    public class JournalEntry
+    {
+        public Guid Id { get; set; }
+        public Guid CampaignId { get; set; }
+        public string OwnerUserId { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
