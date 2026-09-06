4129a89b5b19946d578bfcb0f4663dfa9e3c9bdd
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-06 22:38:47 +0200

    Update JournalEntry.cs
    Opraven překlep

--- Ghoredin/src/Ghoredin.Domain/Journal/JournalEntry.cs
+++ Ghoredin/src/Ghoredin.Domain/Journal/JournalEntry.cs
@@ -11,6 +11,6 @@ namespace Ghoredin.Domain.Journal
         public string OwnerUserId { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdateAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
