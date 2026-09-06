f39cd387548ef50a7ebb2f3247c8dc5f9fec6b3a
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-06 22:39:29 +0200

    Create JournalEntryDto.cs
    Vytvořeno dto pro JournalEntry

---
+++ Ghoredin/src/Ghoredin.Application/Journal/JournalEntryDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Journal
+{
+    public record JournalEntryDto(
+        Guid Id,
+        Guid CampaignId,
+        string Content,
+        DateTime UpdatedAt);
+}
