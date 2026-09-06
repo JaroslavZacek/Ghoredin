6c84d197fd6af5fef1261887aad72000815fb010
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-06 22:35:18 +0200

    Create IJournalRepository.cs
    Vytvořeno rozhraní repositáře deníku.

---
+++ Ghoredin/src/Ghoredin.Application/Journal/IJournalRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ghoredin.Domain.Journal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Journal
+{
+    public interface IJournalRepository
+    {
+        Task<JournalEntry?> GetByOwnerAndCampaignAsync(Guid campaignId, string ownerUserId);
+        Task AddAsync(JournalEntry journalEntry);
+        Task SaveChangesAsync();
+    }
+}
