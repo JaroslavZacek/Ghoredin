f303de091f29e0cac1542579cc8811c9ee4af0a3
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-07 15:21:15 +0200

    Create JournalService.cs
    Vytvořena servisa pro deník

---
+++ Ghoredin/src/Ghoredin.Application/Journal/JournalService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Ghoredin.Application.Campaigns;
+using Ghoredin.Application.Users;
+using Ghoredin.Domain.Journal;
+
+namespace Ghoredin.Application.Journal
+{
+    public class JournalService
+    {
+        private readonly IJournalRepository _journalRepository;
+        private readonly ICampaignRepository _campaignRepository;
+        private readonly ICampaignAuthorizationService _campaignAuthorizationService;
+        private readonly ICurrentUserService _currentUserService;
+
+        public JournalService(
+            IJournalRepository journalRepository,
+            ICampaignRepository campaignRepository,
+            ICampaignAuthorizationService campaignAuthorizationService,
+            ICurrentUserService currentUserService)
+        {
+            _journalRepository = journalRepository;
+            _campaignRepository = campaignRepository;
+            _campaignAuthorizationService = campaignAuthorizationService;
+            _currentUserService = currentUserService;
+        }
+
+        
+
+    }
+}
