c92c62f517bb64903c6af1f8e3e5a752053f63e2
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-07 17:15:06 +0200

    Create JournalRepository.cs
    Vytvořen reposidář deníku a přidána metoda GetByOwnerAndCampaignAsync

---
+++ Ghoredin/src/Ghoredin.Infrastructure/Persistence/Repositories/JournalRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Infrastructure.Persistence.Repositories
+{
+    internal class JournalRepository
+    {
+    }
+}
