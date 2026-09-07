28a09fbb07df9a2cd40ef871caecd992e8508eb1
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-07 08:31:30 +0200

    Create IJournalService.cs
    Vytvoření rozhraní pro servisu deníku

---
+++ Ghoredin/src/Ghoredin.Application/Journal/IJournalService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Journal
+{
+    public interface IJournalService
+    {
+        Task<JournalEntryDto> SaveAsync(SaveJournalCommand command);
+        Task<JournalEntryDto> GetMyEntryAsync(Guid campaignId);
+    }
+}
