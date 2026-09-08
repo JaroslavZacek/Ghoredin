8e654f0095a509294bd0f9a8ab577cce831c8904
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-08 14:17:35 +0200

    Create JournalController.cs
    Vytvořen JournalController a první crud metoda na získání deníku hráče

---
+++ Ghoredin/src/Ghoredin.Server/Controllers/JournalController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using Ghoredin.Application.Journal;
+
+namespace Ghoredin.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class JournalController : ControllerBase
+    {
+        private readonly IJournalService _journalService;
+
+        public JournalController(IJournalService journalService)
+        {
+            _journalService = journalService;
+        }
+
+        #region Get
+
+        [HttpGet("campaign/{campaignId:guid}")]
+        public async Task<IActionResult> GetMine(Guid campaignId)
+        {
+            try
+            {
+                var entry = await _journalService.GetMyEntryAsync(campaignId);
+
+                return Ok(entry);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+
+        #endregion
+    }
+}
