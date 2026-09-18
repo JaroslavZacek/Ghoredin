2669ec3796e020934b0993a41ddb0c2d2bc3a6a4
JaroslavZacek <jaroslav.zacek1991@gmail.com>
2026-09-18 22:17:35 +0200

    Create VoiceParticipantState.cs
    Vytvoření entity, která zmizí po použití při připojení hlasovích kanálů

---
+++ Ghoredin/src/Ghoredin.Application/Voice/VoiceParticipantState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ghoredin.Application.Voice
+{
+    public class VoiceParticipantState
+    {
+        public string UserId { get; set; } = string.Empty;
+        public bool IsSelfMuted { get; set; }
+        public bool IsForceMuted { get; set; }
+        public bool IsSpeaking { get; set; }
+    }
+}
